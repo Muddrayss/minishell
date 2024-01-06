04eabfe39b7de320322efd9c5d07a9f9c72ce784
Claudio Raimondi <claudio.raimondi@protonmail.com>
2024-01-07 00:03:35 +0100

    Add input documentation

--- parser.h
+++ parser.h
@@ -6,7 +6,7 @@
 /*   By: craimond <bomboclat@bidol.juis>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 17:58:23 by craimond          #+#    #+#             */
-/*   Updated: 2024/01/06 18:03:00 by craimond         ###   ########.fr       */
+/*   Updated: 2024/01/06 23:49:02 by craimond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,34 @@
 typedef struct s_parser
 {
     char    **cmd_args; //include il primo (comando effettivo) + argomenti
-    int     fds[2]; //input e output
+
     
 }               t_parser;
 
+/*
+
+casi input:
+
+- (<< limiter cmd)
+- (infile < cmd)
+- (cmd <&n)             prende come input al comando il fd numero n
+
+casi extra:
+
+- (n3< infile.txt)       apre infile.txt sul fd n per un utilizzo futuro
+- (n<&-)                chiude il fd numero n
+
+casi output:
+
+- (cmd > outfile)
+- (cmd >> outfile)
+- (cmd >&n)
+- (cmd >>&n)
+
+n deve essere compreso tra 0 e 1023 incluso (fdmax), altrimenti e' interpretato come argomento e non fd
+un numero n superiore al fdmax e' interpretato come argomento ANCHE SE NON CI SONO SPAZI prima del '>'
+
+il parser si puo accorgere di questi casi specifici perche' la stringa dopo il '>' mantiene o meno gli spazi/tabs
+
+*/
 #endif
