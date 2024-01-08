70381dd1e145e3190c77f1678f69b4fd93f3c961
Claudio Raimondi <craimond@c1r3p7.42firenze.it>
2024-01-08 19:27:28 +0100

    Remove unnecessary checks

--- lexer.c
+++ lexer.c
@@ -6,7 +6,7 @@
 /*   By: craimond <bomboclat@bidol.juis>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 12:03:17 by craimond          #+#    #+#             */
-/*   Updated: 2024/01/07 18:21:03 by craimond         ###   ########.fr       */
+/*   Updated: 2024/01/08 19:25:50 by craimond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 static t_token  lexer_get_token(char c);
 static bool 	is_token(char *str, bool is_first);
-static bool		is_num_before(char *str);
 
 //il lexer separa i token (> < e |) dal resto del comando ("ls -a -l ") che e' tutto insieme compresi spazi successivi
 //dopo andra' fatto uno split/trim del comando prima di execve
@@ -25,13 +24,13 @@ t_list	**lexer(char *input)
 {
     unsigned int    i;
 	bool			is_first;
-    t_lexer   		*elem;
+    t_lexer   		*content;
     t_list         	**lexered_params_head;
 
 	lexered_params_head = (t_list **)malloc(sizeof(t_list *));
 	if (!lexered_params_head)
 		ft_quit(9, "failed to allocate memory");
-	lexered_params_head = NULL;
+	*lexered_params_head = NULL;
 	is_first = true;
     while (*input != '\0')
     {
@@ -40,22 +39,25 @@ t_list	**lexer(char *input)
             i++;
         if (i > 0)
         {
-			elem = (t_lexer *)malloc(sizeof(t_lexer));
-            if (!elem)
+			content = (t_lexer *)malloc(sizeof(t_lexer));
+            if (!content)
                 ft_quit(7, "failed to allocate memory");
-			elem->type = CMD;
-			elem->str.cmd = (char *)malloc(sizeof(char) * i + 1);
-			if (!elem->str.cmd)
+			content->type = CMD;
+			content->str.cmd = (char *)malloc(sizeof(char) * i + 1);
+			if (!content->str.cmd)
 				ft_quit(8, "failed to allocate memory");
-            ft_strlcpy(elem->str.cmd, input, i + 1);
-			ft_lstadd_back(lexered_params_head, ft_lstnew(elem));
+            ft_strlcpy(content->str.cmd, input, i + 1);
+			ft_lstadd_back(lexered_params_head, ft_lstnew(content));
             input += i;
             i = 0;
         }
-		elem = (t_lexer *)malloc(sizeof(t_lexer));
-		elem->type = TOKEN;
-        elem->str.token = lexer_get_token(*input++);
-		ft_lstadd_back(lexered_params_head, ft_lstnew(elem));
+		if (*input != '\0')
+		{
+			content = (t_lexer *)malloc(sizeof(t_lexer));
+			content->type = TOKEN;
+       	 	content->str.token = lexer_get_token(*input++);
+			ft_lstadd_back(lexered_params_head, ft_lstnew(content));
+		}
 		is_first = false;
     }
 	return (lexered_params_head);
@@ -65,7 +67,7 @@ static t_token lexer_get_token(char c)
 {
     int8_t         	i;
 	static uint8_t	n_tokens;
-    static t_token  tokens[4][2] = {
+    static int		tokens[4][2] = {
     {'|', PIPE},
     {'>', REDIR_R},
     {'<', REDIR_L},
@@ -92,27 +94,7 @@ static bool is_token(char *str, bool is_first)
    	while (++i < n_tokens)
 	{
 		if (tokens[i] == *str)
-		{
-			//per evitare i casi tipo (cmd arg1>) e (> out)
-			if (*str == '>' && !(is_first == true || is_shell_space(str[-1]) == true || is_num_before(str - 1) == true))
-				return (false);
 			return (true);
-		}
 	}
-	return (false);
-}
-
-static bool	is_num_before(char *str)
-{
-	unsigned int	i;
-
-	i = 0;
-	while (*str >= '0' && *str <= '9')
-	{
-		str--;
-		i++;
-	}
-	if (i > 0 && is_shell_space(*str) == true)
-		return (true);
 	return (false);
 }
