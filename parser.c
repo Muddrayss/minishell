9284a5433337a1d0fa25e179ca2d9c2b62efed39
Claudio Raimondi <claudio.raimondi@protonmail.com>
2024-01-10 01:28:48 +0100

    Add start of parser logic for <

--- parser.c
+++ parser.c
@@ -6,21 +6,19 @@
 /*   By: craimond <bomboclat@bidol.juis>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 17:58:27 by craimond          #+#    #+#             */
-/*   Updated: 2024/01/09 19:55:32 by craimond         ###   ########.fr       */
+/*   Updated: 2024/01/10 01:27:47 by craimond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static t_parser	*new_elem(t_data *data);
-static void		remove_near_redirs(char *str, unsigned int *cmd_len);
-
 t_list	*parser(t_list *lexered_params, t_data *data)
 {
 	t_list			*parsed_params;
 	t_parser		*content_par;
-	t_lexer			content_lex_prev;
+	t_lexer			*prev_cmd_elem;
 	t_lexer			*content_lex;
+	t_lexer			*next_cmd_elem;
 	char			*cmd_str;
 	size_t			size;
 
@@ -35,38 +33,112 @@ t_list	*parser(t_list *lexered_params, t_data *data)
 		if (content_lex->type == CMD)
 		{
 			ft_strlcat(cmd_str, content_lex->str.cmd, size);
-			content_lex_prev = *content_lex;
+			prev_cmd_elem = content_lex;
 		}
 		else if (content_lex->type == TOKEN)
 		{
+			next_cmd_elem = get_next_cmd_elem(lexered_params);
 			if (content_lex->str.token == PIPE)
 			{
 				ft_lstadd_back(parsed_params, ft_lstnew(content_par));
 				content_par = new_elem(data);
 			}
 			else if (content_lex->str.token == REDIR_L)
-				handle_redir_l(content_lex_prev.str.cmd, content_lex, &content_par, data);
+				handle_redir_l(prev_cmd_elem->str.cmd, next_cmd_elem->str.cmd, content_par, data);
 		}
 		lexered_params = lexered_params->next;
 	}
 	return (parsed_params);
 }
 
-static void	handle_redir_l(char *prev_cmd_str, t_parser *content_par, t_data *data)
+static t_lexer	*get_next_cmd_elem(t_list *lexered_params)
+{
+	t_lexer			*content_lex;
+
+	while (lexered_params)
+	{
+		content_lex = (t_lexer *)lexered_params->content;
+		if (content_lex->type == CMD)
+			return (content_lex);
+		lexered_params = lexered_params->next;
+	}
+	return (NULL);
+}
+
+static void	handle_redir_l(char *prev_cmd,  char *next_cmd, t_parser *content_par, t_data *data)
 {
 	t_redir	*redir_content;
 
 	redir_content = (t_redir *)malloc(sizeof(t_redir));
 	if (!redir_content)
 		ft_quit(12, "failed to allocate memory", data);
-	init_redir(redir_content);
-	while (ft_isdigit(*prev_cmd_str))
-		prev_cmd_str--;
-	if (is_shell_space(*prev_cmd_str))
-		redir_content->fds[0] = ft_atoi(prev_cmd_str);
-	//fare il controllo anche sul next
+	redir_content->fds[0] = -42;
+	redir_content->fds[1] = STDIN_FILENO;
+	add_left_right_fds(&redir_content->fds, prev_cmd, PREV, data);
+	add_left_right_fds(&redir_content->fds, next_cmd, NEXT, data);
 	content_par->redirections = NULL;
 	ft_lstadd_front(&content_par->redirections, ft_lstnew(redir_content));
+}
+
+static void	add_left_right_fds(int *fds[], char *cmd, uint8_t flag, t_data *data)
+{
+	unsigned int	i;
+
+	if (flag == PREV)
+	{
+		i = ft_strlen(cmd) - 1;
+		while (cmd[i] && ft_isdigit(cmd[i]))
+			i--;
+	}
+	else
+	{
+		i = 0;
+		if (cmd[i] == '&')
+			i++;
+		while (cmd[i] && ft_isdigit(cmd[i]))
+			i++;
+	}
+	if (is_shell_space(cmd[i]))
+		(*fds)[flag] = ft_atoi(cmd + i * (flag == PREV));
+}
+
+static void	add_filename_fds(int *fds[], char *cmd, uint8_t flag, t_data *data)
+{
+	unsigned int	i;
+	char			*tmp;
+	char			*filename;
+
+	if (flag == PREV)
+	{
+		i = ft_strlen(cmd) - 1;
+		while (cmd[i] && is_shell_space(cmd[i]))
+			i--;
+		while (cmd[i] && !is_shell_space(cmd[i]))
+			i--;
+		tmp = ft_strdup(&cmd[i + 1]);
+		if (!tmp)
+			ft_quit(16, "failed to allocate memory", data);
+		filename = ft_strtrim(tmp, " \n\t");
+		free(tmp);
+		if (!filename)
+			ft_quit(16, "failed to allocate memory", data);
+	}
+	else if (flag == NEXT)
+	{
+		i = 0;
+		while (cmd[i] && is_shell_space(cmd[i]))
+			i++;
+		filename = ft_strdup(&cmd[i]);
+		if (!filename)
+			ft_quit(16, "failed to allocate memory", data);
+		while (cmd[i] && !is_shell_space(cmd[i]))
+			i++;
+		filename[i] = '\0';
+	}
+	(*fds)[flag] = open(filename, O_RDWR | O_CREAT);
+	free(filename);
+	if ((*fds)[flag] == -1)
+		ft_putstr_fd("No such file or directory", 1);
 }
 
 static unsigned int	get_len(t_list *lexered_params)
--- parser.h
+++ parser.h
@@ -6,12 +6,15 @@
 /*   By: craimond <bomboclat@bidol.juis>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/06 17:58:23 by craimond          #+#    #+#             */
-/*   Updated: 2024/01/09 19:51:19 by craimond         ###   ########.fr       */
+/*   Updated: 2024/01/10 01:19:42 by craimond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PARSER_H
 # define PARSER_H
+
+# define PREV 0
+# define NEXT 1
 
 typedef enum e_redirection_type
 {
@@ -22,7 +25,7 @@ typedef enum e_redirection_type
     REDIR_INPUT_FD,             // '<&n'
     REDIR_OUTPUT_FD,            // '>&n'
     REDIR_READ_WRITE,           // '<>'
-    REDIR_READ_WRITE_FD,         // 'n<>'
+    REDIR_READ_WRITE_FD,        // 'n<>'
     REDIR_FD_DUP_AND_CLOSE,     // 'n>&n'
     REDIR_FD_DUP,               // 'n>&m-'
     REDIR_APPEND_FD,            // '>>&n'
