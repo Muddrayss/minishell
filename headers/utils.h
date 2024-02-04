a695b92344c5167e5a4c5ae5566e3e8e8e8545a6
Raimo <104778891+Raimo33@users.noreply.github.com>
2024-02-04 10:36:57 +0000

    Add malloc protection

--- headers/utils.h
+++ headers/utils.h
@@ -13,24 +13,27 @@
 #ifndef UTILS_H
 # define UTILS_H
 
-char	*ft_strcpy(char *dest, char *src);
-char	*ft_strcat(char *dest, char *src);
-int     ft_strlen(char *str);
 void    ft_putstr_fd(char *s, int fd);
-char	**ft_split(const char *s, char c);
-char	*ft_strjoin(const char *s1, const char *s2);
-char	*ft_strdup(const char *s);
+char    *ft_strdup(const char *s);
 char	**ft_strarr_dup(char **arr);
+char	**ft_split(const char *s, char c);
+size_t	ft_strlen(const char *s);
+char    *ft_strjoin(char const *s1, char const *s2);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
+char    *ft_strcpy(char *dst, const char *src);
+char    *ft_strcat(char *dst, const char *src);
 
+void    *ft_calloc(size_t count, size_t size);
 void	ft_freematrix(char **matrix);
 
-t_list	*lstnew(void *content);
+t_list	*lstnew_p(void *content);
 void	lstadd_back(t_list **lst, t_list *new);
 t_list	*lstlast(t_list *lst);
+void	lstclear(t_list **lst, void (*del)(void *));
 
-t_tree	*treenew(int8_t type, char *cmd);
+t_tree	*treenew_p(int8_t type, char *cmd);
 void    treeadd_below(t_tree **head, t_tree *new);
 t_tree	*tree_last(t_tree *head);
+void    treeclear(t_tree **head, void (*del)(void *));
 
 #endif
