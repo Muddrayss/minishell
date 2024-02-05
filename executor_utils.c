eabfcc66786f2cec939ae5f15fb6eb2be90349aa
Claudio Raimondi <claudio.raimondi@protonmail.com>
2024-02-05 11:51:14 +0100

    remove old replace_env_vars

--- executor_utils.c
+++ executor_utils.c
@@ -6,44 +6,13 @@
 /*   By: craimond <bomboclat@bidol.juis>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/29 20:59:44 by craimond          #+#    #+#             */
-/*   Updated: 2024/02/04 18:44:25 by craimond         ###   ########.fr       */
+/*   Updated: 2024/02/05 11:50:36 by craimond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers/minishell.h"
 
 static char *get_env_name(char *str);
-
-void    replace_env_vars(char **str)
-{
-    char				*env_name;
-    int                 env_name_len;
-    char				*env_value;
-    unsigned int		i;
-
-    i = 0;
-    while ((*str)[i] != '\0')
-    {
-        if ((*str)[i] == '$')
-        {
-            env_name = get_env_name(*str + i + 1); //fino a spazio, '\0' o '$'
-            if (env_name)
-            {
-                if (ft_strncmp(env_name, "?", 2) != 0)
-                    env_value = ft_getenv(env_name);
-                else
-                    env_value = ft_itoa(g_status);
-                env_name_len = ft_strlen(env_name);
-                *str = ft_insert_str(*str, env_value, i, i + env_name_len + 1);
-                if (!*str)
-                    ft_quit(3, "failed to allocate memory");
-                free(env_name);
-                i += env_name_len;
-            }
-        }
-        i++;
-    }
-}
 
 char    *replace_env_vars(char *str)
 {
