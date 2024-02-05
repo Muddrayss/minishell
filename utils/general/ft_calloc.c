4d8a4d6018a26051f5a4b35aae401b050b7bb22d
Claudio Raimondi <claudio.raimondi@protonmail.com>
2024-02-05 12:07:04 +0100

    Fix makefile errors

---
+++ utils/general/ft_calloc.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: craimond <bomboclat@bidol.juis>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/05 12:04:47 by craimond          #+#    #+#             */
+/*   Updated: 2024/02/05 12:06:17 by craimond         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../headers/minishell.h"
+
+static void ft_bzero(void *s, size_t n);
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*arr;
+
+	if (nmemb != 0 && size > ULONG_MAX / nmemb)
+		return (NULL);
+	arr = (void *)malloc(size * nmemb);
+	if (arr == NULL)
+		return (NULL);
+	ft_bzero(arr, size * nmemb);
+	return (arr);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	char	*ptr;
+
+	ptr = (char *)s;
+	while (n-- > 0)
+		ptr[n] = '\0';
+}
