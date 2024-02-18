225563fd851c9855c174ae7ba45a1b599b272050
Ennio Gualandi <egualand@c1r3p2.42firenze.it>
2024-02-18 13:54:34 +0100

    Fix ft_exit to handle all cases. Add function to check if the exit num overflow a long type. Fix exit value if no args are passed

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -3,32 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: craimond <bomboclat@bidol.juis>            +#+  +:+       +#+        */
+/*   By: egualand <egualand@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 18:15:19 by egualand          #+#    #+#             */
-/*   Updated: 2024/02/17 15:18:11 by craimond         ###   ########.fr       */
+/*   Updated: 2024/02/18 13:52:57 by egualand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 
 static bool	is_number(char *str);
+static bool is_long_type_overflow(char *str);
 
 void ft_exit(char **args)
 {
 	if (!args[1])
-		exit(EXIT_SUCCESS);
+		exit(g_status);
 	if (args[2])
 	{
 		ft_putstr_fd("exit: too many arguments\n", STDERR_FILENO);
 		exit(EXIT_FAILURE);
 	}
-	if (is_number(args[1]))
-		exit((uint8_t)ft_atou(args[1])); //non basta atoi, va paddato in un range (overflow underflow)
+	if (is_number(args[1]) && !is_long_type_overflow(args[1]))
+		exit((uint8_t)ft_atou(args[1]));
 	ft_putstr_fd("exit: ", STDERR_FILENO);
 	ft_putstr_fd(args[1], STDERR_FILENO);
 	ft_putstr_fd(": numeric argument required\n", STDERR_FILENO);
-	exit(2); //nel mio bash da casa è 2, non 255
+	exit(2);
 }
 
 static bool	is_number(char *str)
@@ -42,4 +43,20 @@ static bool	is_number(char *str)
 		str++;
 	}
 	return (true);
+}
+
+static bool	is_long_type_overflow(char *str)
+{
+	if (*str == '-' || *str == '+')
+	{
+		str++;
+		if (ft_strcmp(str, "9223372036854775808") > 0)
+			return (true);
+	}
+	else
+	{
+		if (ft_strcmp(str, "9223372036854775807") > 0)
+			return (true);
+	}
+	return (false);
 }
