2a82b0ea7b0deefe82724796256aa9b1c9e9e643
Ennio Gualandi <egualand@c1r3p2.42firenze.it>
2024-02-18 14:32:52 +0100

    Refractor ft_cd. Now it uses sjust one function to change and update the env vars. Fix ft_cd too, initially it did not update the env vars correctly

--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -3,25 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_cd.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: craimond <bomboclat@bidol.juis>            +#+  +:+       +#+        */
+/*   By: egualand <egualand@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 15:08:20 by egualand          #+#    #+#             */
-/*   Updated: 2024/02/17 15:04:41 by craimond         ###   ########.fr       */
+/*   Updated: 2024/02/18 14:31:17 by egualand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 
-static char *get_absolute_from_relative_path(char *path);
-static void update_env_vars(char *new_path);
-
 void ft_cd(char **args)
 {
+	char *cwd;
+
 	if (args[2])
 	{
 		ft_putstr_fd("cd: too many arguments\n", STDERR_FILENO);
 		exit(EXIT_FAILURE);
 	}
+	cwd = getcwd(NULL, 0);
 	if (chdir(args[1]) == -1)
 	{
 		//forse si piò usare perror direttamente
@@ -32,51 +32,9 @@ void ft_cd(char **args)
 		ft_putstr_fd("\n", STDERR_FILENO);
 		exit(EXIT_FAILURE);
 	}
-	update_env_vars(args[1]);
+	ft_setenv("OLDPWD", cwd, true);
+	free(cwd);
+	cwd = getcwd(NULL, 0);
+	ft_setenv("PWD", cwd, true);
 	exit(EXIT_SUCCESS);
-}
-
-static char *get_absolute_from_relative_path(char *path)
-{
-	char	*buf;
-	char	*new_path;
-
-	buf = getcwd(NULL, 0);
-	if (buf == NULL)
-	{
-		perror("cd: Error:");
-		exit(EXIT_FAILURE);
-	}
-	new_path = ft_strjoin(buf, "/");
-	free(buf);
-	if (new_path == NULL)
-		return (NULL);
-	buf = ft_strjoin(new_path, path);
-	free(new_path);
-	return (buf);
-}
-
-static void update_env_vars(char *new_path)
-{
-	char	*oldpwd;
-	char	*pwd;
-	char	*new_path_absolute;
-
-	oldpwd = ft_getenv("OLDPWD");
-	pwd = ft_getenv("PWD");
-	if (new_path[0] == '/')
-		new_path_absolute = ft_strdup(new_path);
-	else
-		new_path_absolute = get_absolute_from_relative_path(new_path);
-	if (oldpwd)
-		ft_setenv("OLDPWD", pwd, true);
-	else
-		ft_setenv("OLDPWD", pwd, false);
-	if (pwd)
-		ft_setenv("PWD", new_path_absolute, true);
-	else
-		ft_setenv("PWD", new_path_absolute, false);
-	free(oldpwd);
-	free(pwd);
-	free(new_path_absolute);
 }
