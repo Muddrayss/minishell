5190f27ec1722b75f52316403c8a2085102a7818
Claudio Raimondi <craimond@c1r4p5.42firenze.it>
2024-02-19 15:44:33 +0100

    Fix cd with no args

--- srcs/builtins/ft_cd.c
+++ srcs/builtins/ft_cd.c
@@ -6,39 +6,55 @@
 /*   By: craimond <bomboclat@bidol.juis>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 15:08:20 by egualand          #+#    #+#             */
-/*   Updated: 2024/02/18 22:20:21 by craimond         ###   ########.fr       */
+/*   Updated: 2024/02/19 15:44:02 by craimond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../headers/minishell.h"
 
+static char	*getcwd_p(char *buf, size_t size);
+
 void ft_cd(char **args)
 {
 	char	*cwd;
 
+	if (!args[1])
+		return ;
 	if (args[2])
 	{
 		ft_putstr_fd("cd: too many arguments\n", STDERR_FILENO);
 		g_status = EXIT_FAILURE;
 		return ;
 	}
-	cwd = getcwd(NULL, 0);
+	cwd = getcwd_p(NULL, 0);
+	if (!cwd)
+		return ;
 	if (chdir(args[1]) == -1)
 	{
-		//forse si piò usare perror direttamente
-		ft_putstr_fd("cd: ", STDERR_FILENO);
-		ft_putstr_fd(args[1], STDERR_FILENO);
-		ft_putstr_fd(": ", STDERR_FILENO);
-		ft_putstr_fd(strerror(errno), STDERR_FILENO);
-		ft_putstr_fd("\n", STDERR_FILENO);
+		perror("cd");
 		g_status = EXIT_FAILURE;
 		free(cwd);
 		return ;
 	}
-	ft_setenv("OLDPWD", cwd, true); //se fai la free di cwd, OLDPWD punta a un indirizzo di memoria che non esiste più perche' setenv NON fa una copia della stringa
+	ft_setenv("OLDPWD", cwd, true);
 	free(cwd);
-	cwd = getcwd(NULL, 0);
+	cwd = getcwd_p(NULL, 0);
+	if (!cwd)
+		return ;
 	ft_setenv("PWD", cwd, true);
 	free(cwd);
 	g_status = EXIT_SUCCESS;
+}
+
+static char	*getcwd_p(char *buf, size_t size)
+{
+	char	*cwd;
+
+	cwd = getcwd(buf, size);
+	if (!cwd)
+	{
+		perror("cd");
+		g_status = EXIT_FAILURE;
+	}
+	return (cwd);
 }
